a274d42ef911eee46304d22f510a1cbc576f45be
Jack Schuss <jackschuss@gmail.com>
2016-04-12 07:32:17 -0400

    complete assignment 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
-var pointsArray = document.getElementsByClassName('point');
 var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-
-    function revealPoints (points){
-        for (var i = 0; i < points.length ; i ++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    function revealPoints (index) {
+        pointsArray[index].style.opacity = 1;
+        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 }
-    revealPoints(pointsArray);
+
+    forEach(pointsArray, revealPoints);
 };
 
 window.onload = function() {
