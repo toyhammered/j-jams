caaee87fb89ac1c0948185cca1f445645c660747
Jack Schuss <jackschuss@gmail.com>
2016-04-12 07:10:49 -0400

    scrolling event added

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
+var pointsArray = document.getElementsByClassName('point');
 var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
 
-    function revealPoints (){
+
+    function revealPoints (points){
         for (var i = 0; i < points.length ; i ++){
         points[i].style.opacity = 1;
         points[i].style.transform = "scaleX(1) translateY(0)";
@@ -9,7 +10,18 @@ var animatePoints = function() {
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 }
-
-
-    revealPoints();
+    revealPoints(pointsArray);
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950){
+        animatePoints()
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0]
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints()
+        }
+    });
+}
