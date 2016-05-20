d91c2cd01dec978f7ab928aba1bb90b896e4545e
Jack Schuss <jackschuss@gmail.com>
2016-05-19 23:13:45 -0400

    assignment33

--- scripts/album.js
+++ scripts/album.js
@@ -29,14 +29,14 @@ var createSongRow = function(songNumber, songName, songLength) {
 				$('.main-controls .play-pause').html(playerBarPauseButton);
 				currentSoundFile.play();
 			}
-				else {
-					$(this).html(playButtonTemplate);
-					$('.main-controls .play-pause').html(playerBarPlayButton);
-					currentSoundFile.pause();
-				}
-
+			else {
+				$(this).html(playButtonTemplate);
+				$('.main-controls .play-pause').html(playerBarPlayButton);
+				currentSoundFile.pause();
 			}
-		
+
+		}
+
 
 
 	};
@@ -187,6 +187,20 @@ var previousSong = function(){
 
 };
 
+var togglePlayFromPlayerBar = function () {
+	var currentlyPlayingHtml = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+
+	if (currentSoundFile.isPaused()) {
+		currentlyPlayingHtml.html(pauseButtonTemplate);
+		$playPauseButton.html(playerBarPauseButton);
+		currentSoundFile.play();
+	}
+	else {
+		currentlyPlayingHtml.html(playButtonTemplate);
+		$playPauseButton.html(playerBarPlayButton);
+		currentSoundFile.pause();
+	}
+}
 
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -202,9 +216,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous')
 var $nextButton = $('.main-controls .next')
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
 	setCurrentAlbum(albumPicasso);
 	$previousButton.click(previousSong);
 	$nextButton.click(nextSong);
+	$playPauseButton.click(togglePlayFromPlayerBar);
 });
